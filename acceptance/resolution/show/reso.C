0d5c6aa590df8381e4855088c0b6d62a2b122fe2
maureeungaro <maurizio.ungaro@cnu.edu>
2015-03-19 14:21:57 -0400

    scripts finalized for e1-6 recook

--- acceptance/resolution/show/reso.C
+++ acceptance/resolution/show/reso.C
@@ -6,17 +6,10 @@
 	string  what[2] = {"electrons", "protons" };
 	int WHAT        = 0;  // 0 = electrons; 1 = protons
 
-	string bindex[3] = {"b24", "b48", "b96"};
-	int BIN          = 0;  
+	int SECTOR      = 1;
 	
-	int WW       = 0;
-	int QQ       = 0;
 	string PRINT = "";
 
-	bins *Bin[3];
-	Bin[0] = new bins(45, 10, 24);
-	Bin[1] = new bins(45, 10, 48);
-	Bin[2] = new bins(45, 10, 96);
 	
 	gROOT->LoadMacro("utils.C");
 	
--- acceptance/resolution/show/utils.C
+++ acceptance/resolution/show/utils.C
@@ -1,143 +1,65 @@
-void change_q2()
+void change_sector()
 {
-	bins Bin;
-	QQ++;
-	if(QQ==Bin.Q2BIN-1) QQ=0;
-	cout << endl << " W set to " << Bin.wm_center[WW] << "  Q2 set to " << Bin.q2_center[QQ] << endl << endl;
+	SECTOR++;
+	if(SECTOR == 7) SECTOR = 1;
+		
+	cout << " Sector set to: " << SECTOR << endl;
 }
 
-void change_w()
-{
-	bins Bin;
-	WW++;
-	if(WW==Bin.WMBIN) WW=0;
-	cout << endl << " W set to " << Bin.wm_center[WW] << "  Q2 set to " << Bin.q2_center[QQ] << endl << endl;
-}
-
-void change_what()
+void change_particle()
 {
 	WHAT++;
-	if(WHAT==2) WHAT=0;
-	cout << endl << " WHAT set to " << what[WHAT] << endl << endl;
+	if(WHAT == 2) WHAT = 0;
+	
+	cout << " Sector set to: " << SECTOR << endl;
 }
 
 
-
-void show_phi()
+void show_momentum()
 {
 	gStyle->SetPadLeftMargin(0.18);
 	gStyle->SetPadRightMargin(0.04);
-	if(WHAT == 1)
-		gStyle->SetPadRightMargin(0.12);
 	gStyle->SetPadTopMargin(0.17);
 	gStyle->SetPadBottomMargin(0.14);
 	gStyle->SetFrameFillColor(kWhite);
 	
-	bins Bin;
-
 	TLatex lab;
 	lab.SetTextFont(102);
 	lab.SetTextColor(kBlue+3);
 	lab.SetNDC();
 	
-	TCanvas *PH = new TCanvas("PH","Phi dependence of correction", 800, 800);
-	lab.SetTextSize(0.032);
-	lab.DrawLatex(.04,.94, Form("%s   W=%3.2f  Q^{2}=%3.2f", what[WHAT].c_str(), Bin.wm_center[WW], Bin.q2_center[QQ]) );
-	
-	lab.SetTextColor(kBlack);
-	lab.DrawLatex(.40,.03, Form("#leftarrow   #phi*  #rightarrow") );
+	TCanvas *PH = new TCanvas("PH","Momentum Resolution", 800, 800);
 
-	TPad *PPH   = new TPad("PPH","Phi dependence of correction", 0.01, 0.06, 0.99, 0.88);
+	TPad *PPH   = new TPad("PPH","Momentum Resolution", 0.01, 0.06, 0.99, 0.88);
 	PPH->Draw();
-	PPH->Divide(2, 5);
+	PPH->Divide(2, 3);
 
-	TLegend *lbins  = new TLegend(0.76, 0.90, 1.00, 0.98);
-	lbins->SetBorderSize(0);
-	lbins->SetFillColor(0);
-	if(WHAT==0)
-	{
-		lbins->AddEntry(AH->pi0_phi[WW][QQ][0][2][0], "24 bins", "L");
-		lbins->AddEntry(AH->pi0_phi[WW][QQ][0][2][1], "48 bins", "L");
-		lbins->AddEntry(AH->pi0_phi[WW][QQ][0][2][2], "96 bins", "L");
-	}
-	if(WHAT==1)
-	{
-		lbins->AddEntry(AH->pi0_phi[WW][QQ][0][0][BIN], "Generated", "L");
-		lbins->AddEntry(AH->pi0_phi[WW][QQ][0][1][BIN], "Reconstructed", "L");
-	}
-	lbins->Draw();
 	
-	for(int i=0; i<Bin.CTBIN; i++)
+	for(int s=0; s<6; s++)
 	{
-		PPH->cd(i+1);
+		PPH->cd(s+1);
 
 		// need to update pud here... if I do it
 		// withing the if statement it doesn't work.
-		AH->pi0_phi[WW][QQ][i][0][BIN]->Draw("");
-		gPad->Update();
+		mom[s][0]->Draw("");
 
-		if(WHAT ==0)
-		{
-			AH->pi0_phi[WW][QQ][i][2][2]->Draw("hist 9c");
-			AH->pi0_phi[WW][QQ][i][2][1]->Draw("hist 9csame");
-			AH->pi0_phi[WW][QQ][i][2][0]->Draw("hist 9csame");
-		}
-		
-		if(WHAT == 1)
-		{
-			AH->pi0_phi[WW][QQ][i][0][BIN]->Draw("E ][");
-			double rightmax = 1.1*AH->pi0_phi[WW][QQ][i][1][BIN]->GetMaximum();
-			double scale = gPad->GetUymax()/rightmax;
-			
-			AH->pi0_phi[WW][QQ][i][1][BIN]->Scale(scale);
-			AH->pi0_phi[WW][QQ][i][1][BIN]->Draw("Esame");
-
-			//draw an axis on the right side
-			TGaxis *axis = new TGaxis(gPad->GetUxmax(),gPad->GetUymin(), gPad->GetUxmax(), gPad->GetUymax(),0,rightmax,510,"+L");
-			axis->SetLineColor(kRed+2);
-			axis->SetLabelSize(0.14);
-			axis->SetNdivisions(805);
-			axis->SetLabelOffset(0.03);
-			axis->SetLabelColor(kRed+2);
-			axis->Draw();
-			
-		}
 		
 		lab.SetTextFont(42);
 		lab.SetTextSize(0.17);
 		lab.SetTextColor(kBlack);
 		lab.DrawLatex(.37,.85, Form("cos(#theta*): %2.1f#divide%2.1f", Bin.ct_center[i] - Bin.dct[i]/2.0 , Bin.ct_center[i] + Bin.dct[i]/2.0) );
+
+	
 	}
-	if(PRINT != "" && WHAT==0)
-		PH->Print(Form("pi0_phi_%s_all_W_%3.2f_Q2_%3.2f%s", swhat[WHAT].c_str(), Bin.wm_center[WW], Bin.q2_center[QQ], PRINT.c_str()));
-	if(PRINT != "" && WHAT==1)
-		PH->Print(Form("pi0_phi_%s_%s_W_%3.2f_Q2_%3.2f%s",  swhat[WHAT].c_str(), bindex[BIN].c_str(), Bin.wm_center[WW], Bin.q2_center[QQ], PRINT.c_str()));
+	if(PRINT != "")
+		PH->Print(Form("momentum",  swhat[WHAT].c_str(), bindex[BIN].c_str(), Bin.wm_center[WW], Bin.q2_center[QQ], PRINT.c_str()));
 }
 
 
 
 void print_all()
 {
-	bins Bin;
-	PRINT=".gif";
-	for(int w=0; w<Bin.WMBIN; w++)
-	{
-		WW=w;
-		for(int q=0; q<Bin.Q2BIN-1; q++)
-		{
-			QQ=q;
-			WHAT=1;
-			for(int b=0; b<3; b++)
-			{
-				BIN = b;
-				show_phi();
-			}
-			
-			WHAT=0;
-				show_phi();
-		}
-	}
-	
+	PRINT=".png";
 }
 
 
