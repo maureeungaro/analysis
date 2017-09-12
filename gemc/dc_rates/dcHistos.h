0e260a55394b5b2a1400e1c602a6f4d5bc9aac95
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-09-12 15:32:52 -0400

    more log in the output

--- gemc/dc_rates/dcHistos.h
+++ gemc/dc_rates/dcHistos.h
@@ -3,6 +3,26 @@
 
 
 
+class dcHistos {
+
+public:
+	dcHistos(const string rootFilePath,
+			 const vector<string> confs,
+			 const vector<string> regs,
+			 const vector<string> zones,
+			 int recalc = 0);
+
+
+
+private:
+	double zlimits[2][2][4];  // r,z  --  min,max  --  zone
+
+	int defineHistos();
+	int setHisto
+
+	
+};
+
 
 
 #endif
--- gemc/dc_rates/dcRates.C
+++ gemc/dc_rates/dcRates.C
@@ -1,20 +1,40 @@
+#include "dcHistos.h"
+
+const string rootFilePath = "/opt/root";
+
+// configurations
+const vector<string> confs = { "cadTarget", "clas12NewTarget2"};
+const vector<double> conff = {           1,                  1};  // additional factor to scale the different configurations
+int CONFINDEX = 0;
+
+// region
+const vector<string> regs = {"reg1", "reg2", "reg3"};
+int REGINDEX = 0;
+
+// sectors
+const int NSECT = 6;
+
+// vertex locations
+const vector<string> zones = {"target", "beampipe", "torus", "all"};
+int ZONEINDEX = 0;
 
 
-
-const int NCONF      = 2;
-const string sconf[NCONF]  = {
-	"cadTarget",
-	"clas12NewTarget2"};
-
-const double factor[NCONF] = {1, 1};  // additional factor to scale the different configurations
-
-
-void dcRates(bool recalc)
+void dcRates(bool recalc, bool recompile=false)
 {
+	// first compile the sources
+	// can force recompiling if something changed
+	// otherwise just reload the library
+	// if the library doesn't exist, compile it
+	if(recompile || gSystem->IsFileInIncludePath("dcHistos_cc.so") == false) {
+		gROOT->ProcessLine(".L dcHistos.cc+O");
+	}
 
 	if(recalc) cout << " Re-defining histograms." << endl;
 	else       cout << " Loading histos definitions." << endl;
 
+
+
+	// style
 	gStyle->SetPalette(1);
 	gStyle->SetOptTitle(kWhite);
 	gStyle->SetOptStat(kWhite);
@@ -26,6 +46,12 @@ void dcRates(bool recalc)
 	gStyle->SetCanvasBorderMode(0);
 	gStyle->SetFrameBorderMode(0);
 	gStyle->SetPadBorderMode(0);
+
+
+
+
+
+
 
 
 	TControlBar* bar =
