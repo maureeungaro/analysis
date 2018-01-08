c9c403dae29f39cab021791bd7e1b11448522b67
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-01-07 22:03:22 -0500

    improvements to macros

--- gemc/bst_rates/show_BST_rates.C
+++ gemc/bst_rates/show_BST_rates.C
@@ -258,7 +258,7 @@ void show_all_bst_rates()
 		
 	}
 
-	TLegend *tmodels  = new TLegend(0.46, 0.5, 0.85, 0.82);
+	TLegend *tmodels  = new TLegend(0.06, 0.5, 0.35, 0.82);
 	for(int t=0; t<NTARGET; t++)
 		tmodels->AddEntry(bst_pr[layer][ENERGY][t], Form("%s", starget[t].c_str()),     "F");
 	
